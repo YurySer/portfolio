226d5fbaebc727f46808ccf2c97fe5d5ea1b289f
YurySer <104967231+YurySer@users.noreply.github.com>
2025-02-10 18:47:36 +0300

    Updates

--- assets/extends-EWcKIc6y.js
+++ assets/extends-EWcKIc6y.js
@@ -1,4 +1,4 @@
-import{r as Ze,g as mx,j as In}from"./index-CxBZPShT.js";/**
+import{r as Ze,g as mx,j as In}from"./index-DI30MEMw.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
